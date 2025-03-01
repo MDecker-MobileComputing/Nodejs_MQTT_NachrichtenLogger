726eef751b7437ba5767b4fa6957e7bbd4c14cd1
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-03-01 17:06:37 +0100

    datei-anhaenger.js: Kommentare dazu

--- src/datei-anhaenger.js
+++ src/datei-anhaenger.js
@@ -1,7 +1,21 @@
 import { promises as fs } from "fs";
 
+
+/**
+ * Ein Objekt dieser Klasse repräsentiert eine Textdatei, an die durch Aufruf der Methode
+ * `zeileAnDateiAnhaengen()` Zeilen angehängt werden können. Diese Zeilen sollen im 
+ *  vorliegenden Fall Schlagzeilen sein.
+ */
 export default class DateiAnhaenger {
 
+    /**
+     * Objekt erstellen und Dateinamen festlegen.
+     * 
+     * @param {string} prefix - Präfix für den Dateinamen, z.B. "schlagzeilen-inland";
+     *                          an das Präfix wird das Datum und die Uhrzeit angehängt,
+     *                          die Datei bekommt die Endung ".txt".
+     *                          Beispielname: "schlagzeilen-inland-2021-12-31_23-59-59.txt"
+     */
     constructor( prefix ) {
 
         const now = new Date();
@@ -12,6 +26,12 @@ export default class DateiAnhaenger {
         console.log( `Neue Datei: ${this.dateiname}` );
     }
 
+
+    /**
+     * Eine Zeile an Textdatei anhängen.
+     * 
+     * @param {string} zeile - Zeile mit Negativschlagzeile, die an die Datei angehängt werden soll
+     */
     async zeileAnDateiAnhaengen( zeile ) {
 
         try {
